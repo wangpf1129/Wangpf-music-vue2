6688004fc34715f33e3eaa355bf2fa8cc94b47d3
Wangpf <1204981117@qq.com>
2021-04-16 14:30:43 +0800

    歌手页面中的歌手分类功能

---
+++ src/components/Home/Singers/SingersCategory.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="singers-category-wrapper">
+    <div v-for="(value,key) of category" :key="key">
+      <ul class="category">
+        <li v-for="(item,index) of value"
+            :key="item.id"
+            :class="['category-item',{'category-active':activeIndex[key]===index}]"
+            @click="activeIndex[key] = index"
+        >
+          {{ item.name }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SingersCategory',
+  props: {
+    category: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      activeIndex: {
+        sex: 0,
+        genre: 0,
+        area: 0
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.singers-category-wrapper {
+  .category {
+    display: flex;
+    flex-wrap: wrap;
+    color: #afafaf;
+    
+    > .category-item {
+      margin: 6px;
+      padding: 4px 6px;
+      
+      &.category-active {
+        color: #fff;
+        background-color: #1a73e8;
+        border-radius: 10px;
+      }
+    }
+  }
+}
+</style>
--- src/views/Singers.vue
+++ src/views/Singers.vue
@@ -1,12 +1,33 @@
 <template>
-<div>
-  歌手页面
-</div>
+  <div class="singers-wrapper">
+    <singers-category :category="singersCategory"/>
+  </div>
 </template>
 
 <script>
+import SingersCategory from '@/components/Home/Singers/SingersCategory';
+
 export default {
-  name: 'Singers'
+  name: 'Singers',
+  components: {SingersCategory},
+  data() {
+    return {
+      singersCategory: {}
+    };
+  },
+  methods: {
+    async fetchSingersCategory() {
+      const {data: {data: category}} = await this.$http.get('/singer/category');
+      this.singersCategory = {
+        sex: category.sex,
+        area: category.area,
+        genre: category.genre
+      };
+    }
+  },
+  created() {
+    this.fetchSingersCategory();
+  }
 };
 </script>
 
