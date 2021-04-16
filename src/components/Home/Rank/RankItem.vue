4f39d1cd3ad792e7481362b0ab82362408d9f943
Wangpf <1204981117@qq.com>
2021-04-16 15:53:11 +0800

    抽离排行榜组件，并使用scroll

---
+++ src/components/Home/Rank/RankItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="rank-item-wrapper">
+    <div class="rank-item" v-for="rank of rankList" :key="rank.topId">
+      <img class="rank-img" :src="rank.picUrl" alt="">
+      <ul>
+        <li v-for="(song,index) of rank.song" :key="index">
+          {{ song.rank }}. {{ song.title }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RankItem',
+  props: {
+    rankList: {
+      type: Array,
+      required: true
+    }
+  }
+};
+
+</script>
+
+<style lang="scss" scoped>
+.rank-item-wrapper {
+  display: inline-block;
+  margin-bottom: 20px;
+  
+  .rank-item {
+    display: flex;
+    box-shadow: 2px 2px 3px rgba(0, 0, 0, .08);
+    margin: 24px;
+    
+    > .rank-img {
+      width: 100px;
+      border-radius: 10px;
+    }
+    
+    > ul {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      
+      > li {
+        margin-left: 8px;
+        font-size: 14px;
+        font-weight: 700;
+        max-width: 200px;
+        overflow: hidden; /*超出隐藏*/
+        white-space: nowrap; /*强制在同一行显示*/
+        text-overflow: ellipsis; /*省略号*/
+      }
+    }
+  }
+}
+
+</style>
--- src/views/Rank.vue
+++ src/views/Rank.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="rank-wrapper">
-    <div class="rank-item" v-for="rank of rankList" :key="rank.topId">
-      <img class="rank-img" :src="rank.picUrl" alt="">
-      <ul>
-        <li v-for="(song,index) of rank.song" :key="index">
-          {{ song.rank }}. {{ song.title }}
-        </li>
-      </ul>
-    </div>
+    <scroll :list="rankList" :top="100">
+      <div class="scroll-wrapper">
+        <rank-item :rank-list="rankList"/>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script>
+import RankItem from '@/components/Home/Rank/RankItem';
+import Scroll from '@/common/Scroll';
+
 export default {
   name: 'Rank',
+  components: {Scroll, RankItem},
   data() {
     return {
       rankList: []
@@ -23,7 +24,6 @@ export default {
     async fetchSongsRank() {
       const res = await this.$http.get('/top/category', {params: {showDetail: 1}});
       this.rankList = res.data.data.reduce((newValue, item) => newValue.concat(item.list), []);
-      console.log(this.rankList);
     }
   },
   created() {
@@ -34,31 +34,6 @@ export default {
 
 <style scoped lang="scss">
 .rank-wrapper {
-  .rank-item {
-    display: flex;
-    box-shadow: 2px 2px 3px rgba(0, 0, 0, .08);
-    margin: 24px;
-    
-    > .rank-img {
-      width: 100px;
-      border-radius: 10px;
-    }
-    
-    > ul {
-      display: flex;
-      flex-direction: column;
-      justify-content: space-around;
-      
-      > li {
-        margin-left: 8px;
-        font-size: 14px;
-        font-weight: 700;
-        max-width: 200px;
-        overflow: hidden; /*超出隐藏*/
-        white-space: nowrap; /*强制在同一行显示*/
-        text-overflow: ellipsis; /*省略号*/
-      }
-    }
-  }
+
 }
 </style>
