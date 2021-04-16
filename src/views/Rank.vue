ae8c9ebdc1e75f8ccde8ed5651212e387d50bd40
Wangpf <1204981117@qq.com>
2021-04-16 15:47:53 +0800

    排行榜样式

--- src/views/Rank.vue
+++ src/views/Rank.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    排行榜页面
+  <div class="rank-wrapper">
+    <div class="rank-item" v-for="rank of rankList" :key="rank.topId">
+      <img class="rank-img" :src="rank.picUrl" alt="">
+      <ul>
+        <li v-for="(song,index) of rank.song" :key="index">
+          {{ song.rank }}. {{ song.title }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -26,5 +33,32 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.rank-wrapper {
+  .rank-item {
+    display: flex;
+    box-shadow: 2px 2px 3px rgba(0, 0, 0, .08);
+    margin: 24px;
+    
+    > .rank-img {
+      width: 100px;
+      border-radius: 10px;
+    }
+    
+    > ul {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      
+      > li {
+        margin-left: 8px;
+        font-size: 14px;
+        font-weight: 700;
+        max-width: 200px;
+        overflow: hidden; /*超出隐藏*/
+        white-space: nowrap; /*强制在同一行显示*/
+        text-overflow: ellipsis; /*省略号*/
+      }
+    }
+  }
+}
 </style>
