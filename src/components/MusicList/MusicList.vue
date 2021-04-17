cfeb305766029c2d84c2bad459b92eff877d2c7a
Wangpf <1204981117@qq.com>
2021-04-17 16:41:17 +0800

    获取音乐列表数据

---
+++ src/components/MusicList/MusicList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="music-list-wrapper">
+    <div class="back">
+      <i class="icon-back">back</i>
+    </div>
+    <h1 class="title">{{ title }}</h1>
+    <div class="bg-image">
+      <img :src="bgUrl" alt="">
+      <div class="filter"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MusicList',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    bgUrl: {
+      type: String,
+      default: ''
+    },
+    songsList: {
+      type: Array,
+      default: [] || null
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.music-list-wrapper {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background-color: #fafafa;
+}
+</style>
--- src/components/SingerDetail/SingerDetail.vue
+++ src/components/SingerDetail/SingerDetail.vue
@@ -1,21 +1,35 @@
 <template>
   <transition name="slide-fade">
-    <div class="singer-detail-wrapper">
-      SingerDetail
-    </div>
+    <music-list :title="title" :songs-list="songsList" :bg-url="bgUrl"></music-list>
   </transition>
 </template>
 
 <script>
 import {mapGetters} from 'vuex';
+import MusicList from '@/components/MusicList/MusicList';
 
 export default {
   name: 'SingerDetail',
+  components: {MusicList},
+  data() {
+    return {
+      songsList: []
+    };
+  },
+  computed: {
+    title() {
+      return this.singer().singer_name;
+    },
+    bgUrl() {
+      return this.singer().singer_pic;
+    }
+  },
   methods: {
     ...mapGetters(['singer']),
     async fetchSingerSongs() {
       const res = await this.$http.get('/singer/songs', {params: {singermid: this.singer().singer_mid,}});
-      console.log(res.data.data);
+      console.log(res.data.data.list);
+      this.songsList = res.data.data.list;
     }
   },
   created() {
@@ -25,22 +39,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-.singer-detail-wrapper {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #fafafa;
-}
-
-
 .slide-fade-enter-active, .slide-fade-leave-active {
-  transition: all .3s;
+  transition: all .6s;
 }
 
 .slide-fade-enter, .slide-fade-leave-to {
   transform: translate3d(100%, 0, 0);
 }
+
 </style>
