a3a7729d6de0f7769a0ea911a1a655ebbf001fa2
Wangpf <1204981117@qq.com>
2021-04-18 14:27:01 +0800

    播放器数据设计

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,3 +1,11 @@
 export const singer = state => state.singer;
 export const songSheet = state => state.songSheet;
 export const rankSheet = state => state.rankSheet;
+
+export const playing = state => state.playing;
+export const fullscreen = state => state.fullscreen;
+export const playList = state => state.playList;
+export const sequenceList = state => state.sequenceList;
+export const mode = state => state.mode;
+export const currentIndex = state => state.currentIndex;
+export const currentSong = state => state.playList[state.currentIndex] || {};
--- src/store/state.js
+++ src/store/state.js
@@ -1,7 +1,15 @@
+import {playMode} from '@/common/JS/config';
+
 const state = {
   singer: {},
-  songSheet:{},
-  rankSheet:{}
+  songSheet: {},
+  rankSheet: {},
+  playing: false, // 是否播放
+  fullscreen: false, // 是否全屏播放
+  playList: [],  // 播放列表
+  sequenceList: [], // 顺序列表
+  mode: playMode.sequence,  // 播放模式
+  currentIndex:-1, // 当前播放歌曲的索引
 };
 
 export default state;
